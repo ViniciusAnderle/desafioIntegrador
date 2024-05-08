bed8d9ca42bf7992a6f7426e1cae250345097dea
Vinicius Anderle <vinianderle00@gmail.com>
2024-05-08 12:00:57 -0300

    comments

--- script.js
+++ script.js
@@ -8,18 +8,14 @@ async function getLastUpdatedTimestamp() {
 // Fetch para obter o timestamp da última atualização e atualizar o rodapé
 getLastUpdatedTimestamp()
   .then(timestamp => {
+      // Formata o timestamp para exibir no formato de data e hora local
       const formattedTimestamp = new Date(timestamp).toLocaleString('pt-BR');
+      // Atualiza o elemento HTML com o timestamp formatado
       document.getElementById('updatedTimestamp').textContent = `Última atualização da API: ${formattedTimestamp}`;
   })
   .catch(error => console.error('Error:', error));
 
-
-
-
-
-
-
-
+// Fetch para obter os dados globais da COVID-19 e atualizar os totais no cabeçalho
 fetch('https://disease.sh/v3/covid-19/all')
   .then(response => response.json())
   .then(data => {
@@ -29,6 +25,7 @@ fetch('https://disease.sh/v3/covid-19/all')
   })
   .catch(error => console.error('Error:', error));
 
+// Fetch para obter os dados dos países e exibir os 10 países com mais casos em uma tabela
 fetch('https://disease.sh/v3/covid-19/countries?sort=cases&yesterday=false&allowNull=false')
   .then(response => response.json())
   .then(data => {
@@ -40,9 +37,11 @@ fetch('https://disease.sh/v3/covid-19/countries?sort=cases&yesterday=false&allow
   })
   .catch(error => console.error('Error:', error));
 
+// Fetch para obter os dados históricos globais da COVID-19 e exibir gráficos de casos e mortes
 fetch('https://disease.sh/v3/covid-19/historical/all?lastdays=all')
   .then(response => response.json())
   .then(data => {
+    // Prepara os dados para o gráfico de casos
     const casesData = {
       labels: Object.keys(data.cases).map(date => new Date(date).toLocaleDateString('pt-BR')),
       datasets: [{
@@ -52,6 +51,7 @@ fetch('https://disease.sh/v3/covid-19/historical/all?lastdays=all')
         borderWidth: 1
       }]
     };
+    // Prepara os dados para o gráfico de mortes
     const deathsData = {
       labels: Object.keys(data.deaths).map(date => new Date(date).toLocaleDateString('pt-BR')),
       datasets: [{
@@ -61,6 +61,7 @@ fetch('https://disease.sh/v3/covid-19/historical/all?lastdays=all')
         borderWidth: 1
       }]
     };
+    // Cria os gráficos de casos e mortes
     const casesCtx = document.getElementById('casesChart').getContext('2d');
     const deathsCtx = document.getElementById('deathsChart').getContext('2d');
     new Chart(casesCtx, {
@@ -74,6 +75,7 @@ fetch('https://disease.sh/v3/covid-19/historical/all?lastdays=all')
   })
   .catch(error => console.error('Error:', error));
 
+// Fetch para obter os dados dos países e exibir um gráfico de pizza com os 10 países com mais casos
 fetch('https://disease.sh/v3/covid-19/countries?sort=cases&yesterday=false&allowNull=false')
   .then(response => response.json())
   .then(data => {
@@ -81,6 +83,7 @@ fetch('https://disease.sh/v3/covid-19/countries?sort=cases&yesterday=false&allow
     const countriesLabels = topCountries.map(country => country.country);
     const countriesCases = topCountries.map(country => country.cases);
 
+    // Cria o gráfico de pizza com os países e seus casos
     const ctx = document.getElementById('topCasesChart').getContext('2d');
     new Chart(ctx, {
       type: 'pie',
@@ -111,14 +114,17 @@ fetch('https://disease.sh/v3/covid-19/countries?sort=cases&yesterday=false&allow
   })
   .catch(error => console.error('Error:', error));
 
+// Fetch para obter os dados dos países e exibir um gráfico de pizza com os 10 países com mais casos, usando a biblioteca Google Charts
 fetch('https://disease.sh/v3/covid-19/countries?sort=cases&yesterday=false&allowNull=false')
   .then(response => response.json())
   .then(data => {
     const topCountries = data.slice(0, 10);
     const chartData = topCountries.map(country => [country.country, country.cases]);
+
     google.charts.load('current', { 'packages': ['corechart'] });
     google.charts.setOnLoadCallback(drawChart);
 
+    // Função para desenhar o gráfico de pizza
     function drawChart() {
       var data = new google.visualization.DataTable();
       data.addColumn('string', 'Country');
@@ -140,6 +146,7 @@ fetch('https://disease.sh/v3/covid-19/countries?sort=cases&yesterday=false&allow
   })
   .catch(error => console.error('Error:', error));
 
+// Cria um mapa Leaflet para visualizar os casos de COVID-19 por país
 const map = L.map('map').setView([0, 0], 2);
 
 // Define os limites para o mapa
@@ -148,15 +155,17 @@ const northEast = L.latLng(90, 180);
 const bounds = L.latLngBounds(southWest, northEast);
 map.setMaxBounds(bounds);
 
-// Se a posição inicial do mapa estiver fora dos limites, ajuste-a para dentro
+// Ajusta a posição inicial do mapa para dentro dos limites
 map.on('drag', function () {
   map.panInsideBounds(bounds, { animate: false });
 });
 
+// Adiciona a camada de mapa OpenStreetMap ao mapa
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-  minZoom: 2, // Defina o zoom mínimo permitido
+  minZoom: 2, // Define o zoom mínimo permitido
 }).addTo(map);
 
+// Função para obter a cor com base no número de casos
 function getColor(cases) {
   return cases > 1000000 ? '#800026' :
     cases > 500000 ? '#BD0026' :
@@ -168,6 +177,7 @@ function getColor(cases) {
                 '#FFEDA0';
 }
 
+// Fetch para obter os dados dos países e adicionar polígonos coloridos ao mapa com informações de casos
 fetch('https://disease.sh/v3/covid-19/countries')
   .then(response => response.json())
   .then(data => {
@@ -177,6 +187,7 @@ fetch('https://disease.sh/v3/covid-19/countries')
         const long = country.countryInfo.long;
         const cases = country.cases;
 
+        // Adiciona um polígono colorido representando os casos no país, com um tooltip informativo
         L.polygon([
           [lat - 0.5, long - 0.5],
           [lat + 0.5, long - 0.5],
