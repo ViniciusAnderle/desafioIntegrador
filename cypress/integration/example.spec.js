2035b0d0015d680d04b7ae69f2dfb75748095cbf
raizen-leticiaalmeida <leticia.almeida@raizen.com>
2024-05-20 21:23:49 -0300

    Adicionando testes e2e Dashboard brasil

--- cypress/integration/example.spec.js
+++
@@ -1,6 +0,0 @@
-describe('Example Test Suite', () => {
-    it('should display the title correctly', () => {
-      cy.visit('https://example.com');
-      cy.title().should('equal', 'Example Domain');
-    });
-  });
