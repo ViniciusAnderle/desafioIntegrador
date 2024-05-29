f92d9772189ab53f11875fe93b807f34142a1df2
leticiaantunesjpeg <leticiaantunes95@outlook.com>
2024-05-28 21:38:30 -0300

    update braza.cy.js

--- cypress/e2e/braza.cy.js
+++ cypress/e2e/braza.cy.js
@@ -1,10 +1,20 @@
 describe("Dashboard Brasil - Testes de Integração", () => {
   beforeEach(() => {
-    cy.visit("http://127.0.0.1:8080/dashboard_brasil/braza.html");
+    cy.visit("http://172.32.92.218:8080/view/dashboard_brasil/braza.html");
+  });
+
+  it(`teste canvas`, () => {
+    cy.get("#casesChart").trigger('keydown', { keyCode: 16 }) 
+  });
+
+  it(`teste titulos inicialização`, () => {
+    cy.get("h5.card-title").contains("Total de Casos").click();
+    cy.get("h5.card-title").contains("Total Recuperados").click();
+    cy.get("h5.card-title").contains("Total de Casos").click();
   });
 
   it(`DADO que a aplicação é iniciada
-      ENTÃO o título e cards devem ser renderizados`, () => {
+        ENTÃO o título e cards devem ser renderizados`, () => {
     cy.get("h1").should("contain", "Painel COVID-19 Brasil");
     cy.get("#totalCases").should("be.visible");
     cy.get("#totalRecovered").should("be.visible");
@@ -12,8 +22,8 @@ describe("Dashboard Brasil - Testes de Integração", () => {
   });
 
   it(`DADO que a aplicação é iniciada
-      E a busca de casos no Brasil é realizada
-      DEVE alterar o conteúdo para locale pt-BR`, () => {
+        E a busca de casos no Brasil é realizada
+        DEVE alterar o conteúdo para locale pt-BR`, () => {
     cy.intercept("https://disease.sh/v3/covid-19/countries/Brazil", {
       fixture: "brazil_data.json",
     }).as("getBrazilData");
@@ -37,8 +47,8 @@ describe("Dashboard Brasil - Testes de Integração", () => {
   });
 
   it(`DADO que a aplicação é iniciada
-      E a busca por todos os dados é realizada
-      DEVE informar a última vez que a API foi atualizada`, () => {
+        E a busca por todos os dados é realizada
+        DEVE informar a última vez que a API foi atualizada`, () => {
     cy.intercept("https://disease.sh/v3/covid-19/all", {
       fixture: "all_data.json",
     }).as("getAllData");
@@ -57,8 +67,8 @@ describe("Dashboard Brasil - Testes de Integração", () => {
   });
 
   it(`DADO que a aplicação é iniciada
-      E a busca por casos do histórico do Brasil é realizada
-      DEVE renderizar o gráfico de casos diários`, () => {
+        E a busca por casos do histórico do Brasil é realizada
+        DEVE renderizar o gráfico de casos diários`, () => {
     cy.intercept(
       "https://disease.sh/v3/covid-19/historical/Brazil?lastdays=all",
       {
@@ -74,8 +84,8 @@ describe("Dashboard Brasil - Testes de Integração", () => {
   });
 
   it(`DADO que a aplicação é iniciada
-      E o botão de ativar Modo Escuro é acionado
-      DEVE ativar o dark-mode e vice-versa`, () => {
+        E o botão de ativar Modo Escuro é acionado
+        DEVE ativar o dark-mode e vice-versa`, () => {
     cy.get("#toggleDarkMode").click();
     cy.get("body").should("have.class", "dark-mode");
     cy.get("#toggleDarkMode").should("contain", "Modo Claro");
@@ -86,8 +96,8 @@ describe("Dashboard Brasil - Testes de Integração", () => {
   });
 
   it(`DADO que a aplicação é iniciada
-      E a busca por casos do histórico do Brasil é realizada
-      DEVE renderizar o gráfico de recuperados versus mortes`, () => {
+        E a busca por casos do histórico do Brasil é realizada
+        DEVE renderizar o gráfico de recuperados versus mortes`, () => {
     cy.intercept(
       "https://disease.sh/v3/covid-19/historical/Brazil?lastdays=all",
       {
@@ -103,13 +113,13 @@ describe("Dashboard Brasil - Testes de Integração", () => {
   });
 
   it(`DADO que a aplicação é iniciada
-      E o botão "Dashboard de Casos" na barra de navegação é clicado
-      DEVE navegar para a página de Dashboard de casos (Dashboard Inicial)`, () => {
+        E o botão "Dashboard de Casos" na barra de navegação é clicado
+        DEVE navegar para a página de Dashboard de casos (Dashboard Inicial)`, () => {
     cy.get(".nav-dashboard_inicial").click();
 
     cy.url().should("include", "/dashboard_inicial.html");
     cy.get("h1").should("contain", "Painel COVID-19");
   });
 
-  
+
 });
